c47178d3d824c91b803c8527d2ff268ca4d6274c
Angelina Staeck <s52741@beuth-hochschule.de>
2014-11-22 19:45:26 +0100

    VideoList started

--- testcases/module_video_item/app.js
+++ testcases/module_video_item/app.js
@@ -3,16 +3,17 @@ requirejs.config({
         "jquery": '../../videochop/js/lib/jquery-2.1.1.min'
     }
 });
-define(["jquery", "../../videochop/js/modules/videoItem"], (function ($, VideoItem) {
+define(["jquery", "../../videochop/js/modules/videoItem", "../../videochop/js/modules/videoList"], (function ($, VideoItem, VideoList) {
     "use strict";
 
         $(document).ready(function() {
 
             console.log($(".btn"));
             var testItem = new VideoItem({});
+            var testList = new VideoList([testItem, testItem]);
             console.log(testItem);
             $(".btn").on("click",function () {
-                $(".test").append(testItem.getMarkUp());
+                $(".test").append(testList.listVideoItems());
             });
             $(".btn2").on("click",function () {
                 testItem.deleteItem();
