da86e53fe474b84943d68646085b265eff11c998
Duve, Michael <michael.duve@bild.de>
2015-01-03 17:12:14 +0100

    Video-Item Test updated and corrected

--- testcases/module_video_item/app.js
+++ testcases/module_video_item/app.js
@@ -1,21 +1,18 @@
 requirejs.config({
+    baseUrl:"../../videochop/js/",
     paths: {
-        "jquery": '../../videochop/js/lib/jquery-2.1.1.min'
+        "jquery": 'lib/jquery-2.1.1.min'
     }
 });
-define(["jquery", "../../videochop/js/modules/videoItem", "../../videochop/js/modules/videoList"], (function ($, VideoItem, VideoList) {
+define(["jquery", "modules/videoItem", "modules/videoList"], (function ($, VideoItem, VideoList) {
     "use strict";
 
         $(document).ready(function() {
 
             var testItem = new VideoItem({});
-            var testList = new VideoList([testItem, testItem]);
-            console.log(testItem);
+
             $(".btn").on("click",function () {
-                $(".test").append(testList.listVideoItems());
-            });
-            $(".btn2").on("click",function () {
-                testItem.deleteItem();
+                $(".test").append(testItem.toString() + "<br>");
             });
 
         });
