b2f9883af52bebc72b21bfbfacaca6bacbb7fdde
Duve, Michael <michael.duve@bild.de>
2015-01-09 14:18:55 +0100

    draganddrop: repaired

--- testcases/drag-drop-updated/app.js
+++ testcases/drag-drop-updated/app.js
@@ -1,37 +1,88 @@
 requirejs.config({
     baseUrl:"../../videochop/js/",
     paths: {
-        "jquery": 'lib/jquery-2.1.1.min',
-        "ui": 'lib/jquery-ui.min'
+        "jquery": "lib/jquery-2.1.1.min",
+        "jqueryui": "lib/jquery-ui.min",
+        "modernizr": "lib/modernizr",
+        "useragent": "lib/ua-parser.min",
+        "filereader": 'lib/filereader',
+        "videoItemLoader": "modules/videoItemLoader",
+        "videoItem": "modules/videoItem",
+        "videoList": "modules/videoList",
+        "popcorn": "lib/popcorn.min",
+        "popcorn-capture": "lib/popcorn.capture"
     },
     shim: {
-        "ui": {
+        "videoItemLoader": {
+            deps: ["jquery", "videoItem", "popcorn", "popcorn-capture"]
+        },
+        "popcorn-capture": {
+            deps: ["popcorn"]
+        },
+        "popcorn": {
             deps: ["jquery"]
+        },
+        "videoItem": {
+            deps: ["jquery"]
+        },
+        "modernizr": {
+            deps: ["jquery"]
+        },
+        "videoList": {
+            deps: ["jquery", "videoItem"]
         }
-    }
+    },
+    waitSeconds: 0
 });
-define(["jquery", "ui", "modules/videoItem", "modules/videoList"], (function ($, ui, VideoItem, VideoList) {
+define(["jquery", "jqueryui", "videoItem", "videoList", "videoItemLoader", "filereader"], (function ($, ui, VideoItem, VideoList, VideoItemLoader, FileReaderJS) {
     "use strict";
 
         $(document).ready(function() {
 
-            var vidItem = new VideoItem({
-
+            /*video list for debug reasons*/
+            var videoList = new VideoList({
+                container: ".file-list"
             });
 
-            console.log(vidItem);
+            /*video item loader for debug reasons*/
+            /**
+             * VideoItemLoader
+             */
+            var moduleVideoItemLoader = new VideoItemLoader({
+                tempWrapper: ".temporary-video",
+                list: videoList
+            });
+
+            var fileReaderOpts = {
+                readAsDefault: 'ArrayBuffer',
+                accept: "video/*",
+                on: {
+                    loadend: function (e, file) {
+                        moduleVideoItemLoader.add({
+                            data: new Uint8Array(e.target.result),
+                            extension: file.extra.extension,
+                            name: file.extra.nameNoExtension,
+                            prettySize: file.extra.prettySize,
+                            size: file.size,
+                            type: file.type
+                        });
+                    }
+                }
+            };
+
+            FileReaderJS.setupInput($(".file-add")[0], fileReaderOpts);
 
             $(".timeline").sortable({
-                revert: true,
-                revertDuration: 10
+                revert: 10,
+                opacity: 0.3,
+                axis: "x"
             });
-
 
             $(".timeline li").resizable({
                 minWidth: 100,
                 maxWidth: 300,
                 handles: "e, w",
-                stop: function( event, ui ) {
+                stop: function(event) {
                     event.target.style.left = 0;
                 }
             });
